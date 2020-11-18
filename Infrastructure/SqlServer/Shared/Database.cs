64786ffe0d9de6abfd0227f0fbc20e9b2d309626
AntiEdge <antoine.bouilliez@hotmail.com>
2020-11-18 20:23:17 +0100

    Change Database.cs

--- Infrastructure/SqlServer/Shared/Database.cs
+++ Infrastructure/SqlServer/Shared/Database.cs
@@ -4,7 +4,8 @@ namespace Infrastructure.SqlServer.Shared
 {
     public static class Database
     {
-        private static readonly string ConnectionString = @"Server=127.0.0.1,1433;Database=pGestionHelha;User id=SA;Password=ingeid/3510";
+        //private static readonly string ConnectionString = @"Server=127.0.0.1,1433;Database=pGestionHelha;User id=SA;Password=ingeid/3510";
+        private static readonly string ConnectionString = @"Server=tcp:projethelha.database.windows.net,1433;Initial Catalog=pGestionHelha;Persist Security Info=False;User ID=Falcon;Password=ingeid/3510;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 
         public static SqlConnection GetConnection()
         {
