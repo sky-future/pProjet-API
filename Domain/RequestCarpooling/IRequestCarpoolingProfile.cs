85f51e21e26296054b71838e948f97a64e32dd60
Falcon <mistert9412@gmail.com>
2020-12-14 00:43:31 +0100

    RequestCarPooling Get By ID

---
+++ Domain/RequestCarpooling/IRequestCarpoolingProfile.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.RequestCarpooling
+{
+    public interface IRequestCarpoolingProfile
+    {
+        public int IdUser { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Telephone { get; set; }
+        public bool Confirmation { get; set; }
+    }
+}
---
+++ pAPI/Controllers/RequestCarpoolingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Application.Services.RequestCarpooling;
+using Application.Services.RequestCarpooling.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pAPI.Controllers
+{
+    [ApiController]
+    [Route("api/requestcarpooling")]
+    public class RequestCarpoolingController : ControllerBase
+    {
+
+        private readonly IRequestCarpoolingService _requestCarpoolingService;
+
+        private RequestCarpoolingController(IRequestCarpoolingService requestCarpoolingService)
+        {
+            _requestCarpoolingService = requestCarpoolingService;
+        }
+
+        [HttpGet]
+        [Route("{idReceiver}")]
+        public ActionResult<OutputDTORequestCarpoolingByID> GetByIdRequest(int idReceiver)
+        {
+            InputDTOGetByIDRequestCarpooling inputDtoGetByIdRequestCarpooling = new InputDTOGetByIDRequestCarpooling
+            {
+                idRequestReceiver = idReceiver
+            }; 
+            Console.WriteLine(inputDtoGetByIdRequestCarpooling.idRequestReceiver);
+            return Ok(_requestCarpoolingService.GetByIdReceiver(inputDtoGetByIdRequestCarpooling));
+        }
+
+    }
+}
